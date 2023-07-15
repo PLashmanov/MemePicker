4c827031bfaf49953b9ad7632c47e03498d7973e
Pavel Lashmanov <plashmanov@gmail.com>
2023-07-15 11:19:04 -0400

    initial project

---
+++ index.js
@@ -0,0 +1,36 @@
+import {catsData} from './data.js'
+const emotionRadios = document.getElementById("emotion-radios")
+
+function getEmotionsArray(cats){
+
+    const emotionsArray = []
+   for (let cat of cats){
+    for (let emotion of cat.emotionTags){
+            emotionsArray.push(emotion)
+        }
+   }
+   return emotionsArray
+}
+
+function renderEmotionsRadio(cats){
+    const emotions = getEmotionsArray(cats)
+    let radioItems = ``
+    for (let emotion of emotions){
+
+        radioItems += `
+            <div class="radio">
+                <label for="${emotion}">${emotion}</label>
+                <input 
+                    type="radio" 
+                    id="${emotion}"
+                    value="${emotion}"
+                    name="emotions"
+                    >
+            </div>
+        `
+        }
+
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadio(catsData)
