5f2a73b454c842fb340f1113f9beb5a217f8d454
Pavel Lashmanov <plashmanov@gmail.com>
2023-07-15 14:50:26 -0400

    Final
    Meme Picker!

--- index.js
+++ index.js
@@ -1,11 +1,76 @@
 import {catsData} from './data.js'
+
+// consts
+
 const emotionRadios = document.getElementById("emotion-radios")
+const getImageBtn  = document.getElementById("get-image-btn")
+const gifsOnlyOption = document.getElementById ("gifs-only-option")
+const memeModal = document.getElementById("meme-modal")
+const memeModalInner = document.getElementById("meme-modal-inner")
+const memeModalCloseBtn = document.getElementById("meme-modal-close-btn")
+
+//event listeners
 
 
+emotionRadios.addEventListener("change", highlightCheckedOption)
+memeModalCloseBtn.addEventListener("click", closeModal)
+getImageBtn.addEventListener('click', renderCat)
 
-emotionRadios.addEventListener("click", function(e){
-    e.target
-})
+//Functions
+
+
+function highlightCheckedOption(e){
+        
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add("highlight") 
+}
+
+
+function closeModal(){
+    memeModal.style.display = "none"
+}
+
+function renderCat(){
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML = `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >`
+        memeModal.style.display = "flex"
+}
+
+function getSingleCatObject(){
+    const catsArray = getMatchingCatsArray()
+        if (catsArray.length === 1){
+            return catsArray[0]
+        }
+        else {
+            const randomNumber = Math.floor(Math.random() * catsArray.length)
+            return catsArray[randomNumber]
+        }
+
+}
+
+function getMatchingCatsArray(){
+    if (document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        console.log(selectedEmotion)
+        const isGif = gifsOnlyOption.checked
+        const matchingCatsArray = catsData.filter(function(cat){
+                if (isGif ){
+                    return cat.emotionTags.includes(selectedEmotion)  && cat.isGif
+                }else {
+                    return cat.emotionTags.includes(selectedEmotion)
+            }
+        })
+        return matchingCatsArray
+    }
+}
 
 function getEmotionsArray(cats){
 
